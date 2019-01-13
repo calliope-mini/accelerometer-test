0b4c05037632474535a02b398c0abc72c4f60d6b
Matthias L. Jugel <leo@thinkberg.com>
2019-01-13 14:41:50 +0100

    accelerometer correct orientation

--- source/AccelerometerTest.cpp
+++ source/AccelerometerTest.cpp
@@ -9,81 +9,93 @@
  */
 
 #include <MicroBit.h>
+#include <BMX055Accelerometer.h>
 
 MicroBit uBit;
 
 void onFreeFall(MicroBitEvent event) {
     (void) event;
-    uBit.radio.datagram.send("EVENT F");
     uBit.display.print("F");
 }
 
 void on2G(MicroBitEvent event) {
     (void) event;
-    uBit.radio.datagram.send("EVENT 2G");
     uBit.display.print("2");
 }
 
 void on3G(MicroBitEvent event) {
     (void) event;
-    uBit.radio.datagram.send("EVENT 3G");
     uBit.display.print("3");
 }
 
 void on6G(MicroBitEvent event) {
     (void) event;
-    uBit.radio.datagram.send("EVENT 6G");
     uBit.display.print("6");
 }
 
 void on8G(MicroBitEvent event) {
     (void) event;
-    uBit.radio.datagram.send("EVENT 8G");
     uBit.display.print("8");
 }
 
 void onShake(MicroBitEvent event) {
     (void) event;
-    uBit.radio.datagram.send("EVENT SHAKE");
     uBit.display.print("S");
 }
 
 void onFaceUp(MicroBitEvent event) {
     (void) event;
-    uBit.radio.datagram.send("EVENT +");
     uBit.display.print("+");
 }
 
 void onFaceDown(MicroBitEvent event) {
     (void) event;
-    uBit.radio.datagram.send("EVENT -");
     uBit.display.print("-");
 }
 
 void onTiltUp(MicroBitEvent event) {
     (void) event;
-    uBit.radio.datagram.send("EVENT UPSIDE");
     uBit.display.print("U");
 }
 
 void onTiltDown(MicroBitEvent event) {
     (void) event;
-    uBit.radio.datagram.send("EVENT UPSIDE DOWN");
     uBit.display.print("D");
 }
 
 void onTiltLeft(MicroBitEvent event) {
     (void) event;
-    uBit.radio.datagram.send("EVENT TILTED LEFT");
     uBit.display.print("L");
 }
 
 void onTiltRight(MicroBitEvent event) {
     (void) event;
-    uBit.radio.datagram.send("EVENT TILTED RIGHT");
     uBit.display.print("R");
 }
 
+void onUpdate(MicroBitEvent event) {
+    (void) event;
+
+    static int cnt = 1;
+    static int x = 0;
+    static int y = 0;
+    static int z = 0;
+
+    x = (x + uBit.accelerometer.getX()) / cnt;
+    y = (y + uBit.accelerometer.getY()) / cnt;
+    z = (z + uBit.accelerometer.getZ()) / cnt;
+    if (++cnt > 100) {
+        cnt = 1;
+        uBit.serial.send("C = [x=");
+        uBit.serial.send(x);
+        uBit.serial.send(", y=");
+        uBit.serial.send(y);
+        uBit.serial.send(", z=");
+        uBit.serial.send(z);
+        uBit.serial.send("]\r\n");
+    }
+
+}
 
 int main(void) {
     uBit.init();
@@ -91,7 +103,6 @@ int main(void) {
     uBit.serial.send("Calliope Accelerometer Test v1.0\r\n");
 
     uBit.messageBus.listen(MICROBIT_ID_GESTURE, MICROBIT_ACCELEROMETER_EVT_FREEFALL, onFreeFall);
-    uBit.messageBus.listen(MICROBIT_ID_GESTURE, MICROBIT_ACCELEROMETER_EVT_2G, on2G);
     uBit.messageBus.listen(MICROBIT_ID_GESTURE, MICROBIT_ACCELEROMETER_EVT_3G, on3G);
     uBit.messageBus.listen(MICROBIT_ID_GESTURE, MICROBIT_ACCELEROMETER_EVT_6G, on6G);
     uBit.messageBus.listen(MICROBIT_ID_GESTURE, MICROBIT_ACCELEROMETER_EVT_8G, on8G);
@@ -102,69 +113,11 @@ int main(void) {
     uBit.messageBus.listen(MICROBIT_ID_GESTURE, MICROBIT_ACCELEROMETER_EVT_TILT_DOWN, onTiltDown);
     uBit.messageBus.listen(MICROBIT_ID_GESTURE, MICROBIT_ACCELEROMETER_EVT_TILT_LEFT, onTiltLeft);
     uBit.messageBus.listen(MICROBIT_ID_GESTURE, MICROBIT_ACCELEROMETER_EVT_TILT_RIGHT, onTiltRight);
+//    uBit.messageBus.listen(MICROBIT_ID_ACCELEROMETER, MICROBIT_ACCELEROMETER_EVT_DATA_UPDATE, onUpdate);
 
-    uBit.radio.enable();
-    uBit.radio.setTransmitPower(7);
-    uBit.radio.setGroup(0b01100110);
-    uBit.radio.datagram.send("ACCEL TEST START");
-    uint16_t lastGesture = 0;
+
+//    new MicroBitAccelerometerService(*uBit.ble, uBit.accelerometer);
     while (1) {
-        uint16_t g = uBit.accelerometer.getGesture();
-        if (lastGesture != g) {
-            switch (g) {
-                case MICROBIT_ACCELEROMETER_EVT_NONE:
-                    break;
-                case MICROBIT_ACCELEROMETER_EVT_FREEFALL:
-                    uBit.radio.datagram.send("LAST F");
-                    printf("FREEFALL\r\n");
-                    break;
-                case MICROBIT_ACCELEROMETER_EVT_2G:
-                    uBit.radio.datagram.send("LAST 2G");
-                    printf("2G\r\n");
-                    break;
-                case MICROBIT_ACCELEROMETER_EVT_3G:
-                    uBit.radio.datagram.send("LAST 3G");
-                    printf("3G\r\n");
-                    break;
-                case MICROBIT_ACCELEROMETER_EVT_6G:
-                    uBit.radio.datagram.send("LAST 6G");
-                    printf("6G\r\n");
-                    break;
-                case MICROBIT_ACCELEROMETER_EVT_8G:
-                    uBit.radio.datagram.send("LAST 8G");
-                    printf("8G\r\n");
-                    break;
-                case MICROBIT_ACCELEROMETER_EVT_SHAKE:
-                    uBit.radio.datagram.send("LAST S");
-                    printf("S\r\n");
-                    break;
-                case MICROBIT_ACCELEROMETER_EVT_FACE_UP:
-                    uBit.radio.datagram.send("LAST +");
-                    printf("+\r\n");
-                    break;
-                case MICROBIT_ACCELEROMETER_EVT_FACE_DOWN:
-                    uBit.radio.datagram.send("LAST -");
-                    printf("-\r\n");
-                    break;
-                case MICROBIT_ACCELEROMETER_EVT_TILT_UP:
-                    uBit.radio.datagram.send("LAST U");
-                    printf("U\r\n");
-                    break;
-                case MICROBIT_ACCELEROMETER_EVT_TILT_DOWN:
-                    uBit.radio.datagram.send("LAST D");
-                    printf("D\r\n");
-                    break;
-                case MICROBIT_ACCELEROMETER_EVT_TILT_LEFT:
-                    uBit.radio.datagram.send("LAST L");
-                    printf("L\r\n");
-                    break;
-                case MICROBIT_ACCELEROMETER_EVT_TILT_RIGHT:
-                    uBit.radio.datagram.send("LAST R");
-                    printf("R\r\n");
-                    break;
-            }
-            lastGesture = g;
-        }
-        uBit.sleep(1);
+        uBit.sleep(100);
     }
 }
